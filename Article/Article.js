acd7caac31c6d118d3d95d1e7cf7246e345c653d
jdspell <spelljd15@gmail.com>
2019-01-30 17:23:30 -0500

    finished article.js file. Tried to add a close button, but was not successful

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,21 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = querySelector(".expandButton");
+
+    // create a reference to the ".expandButton" class.
+    this.expandButton = this.domElement.querySelector(".expandButton");
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    expandButton.addEventListener("click", this.expandArticle.bind(this));
+    this.expandButton.addEventListener("click", () => this.toggleArticle());
+
   }
 
-  expandArticle() {
+  toggleArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    // domElement.classList.toggle(".article-open");
+    this.domElement.classList.toggle("article-open");
   }
 }
 
@@ -28,4 +32,4 @@ NodeList and create a new instance of Article by passing in each article as a pa
 */
 
 let articles = document.querySelectorAll(".article");
-articles.forEach(article => {new Article(article)});
+articles.forEach(article => new Article(article));
